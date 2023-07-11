f2368f09e5d6f3fc2c2b28803967248bd263e72f
Melissa Aquijes <melissa.geral10@gmail.com>
2023-07-11 11:01:17 -0500

    Se agrega vista de login, iconos e imagen para el background.

---
+++ src/views/login.js
@@ -0,0 +1,103 @@
+function login() {
+    const containerLogin = document.createElement('section');
+    const divBackground = document.createElement('div');
+    const blackTransparency = document.createElement('img');
+    const sectionForm = document.createElement('section');
+    const imgLogo = document.createElement('img');
+    const form = document.createElement('form');
+    const divInputUser = document.createElement('div');
+    const spanIconUser = document.createElement('span');
+    const inputIconUser = document.createElement('img');
+    const inputEmail = document.createElement('input');
+    const divInputPassword = document.createElement('div');
+    const spanIconPassword = document.createElement('span');
+    const inputIconPassword = document.createElement('img');
+    const inputPassword = document.createElement('input');
+    const msgEmailPassword = document.createElement('p');
+    const buttonSingIn = document.createElement('button');
+    const sectionLinks = document.createElement('div');
+    const createAccount = document.createElement('p');
+    const forgotPassword = document.createElement('p');
+    const sectionConnectWith = document.createElement('div');
+    const line1 = document.createElement('div');
+    const othersAccess = document.createElement('p');
+    const line2 = document.createElement('div');
+    const sectionIconApp = document.createElement('section');
+    const imgIconFacebook = document.createElement('img');
+    const imgIconGoogle = document.createElement('img');
+    const imgIconTwitter = document.createElement('img');
+
+    containerLogin.classList.add('containerLogin');
+    divBackground.classList.add('divBackground');
+    blackTransparency.classList.add('blackTransparency');
+    sectionForm.classList.add('sectionForm');
+    imgLogo.classList.add('imgLogo');
+    form.classList.add('form');
+    divInputUser.classList.add('divInputUser');
+    spanIconUser.classList.add('spanIconUser');
+    inputIconUser.classList.add('inputIconUser');
+    inputEmail.classList.add('inputEmail');
+    divInputPassword.classList.add('divInputPassword');
+    spanIconPassword.classList.add('spanIconPassword');
+    inputIconPassword.classList.add('inputIconPassword');
+    inputPassword.classList.add('inputPassword');
+    msgEmailPassword.classList.add('msgEmailPassword');
+    buttonSingIn.classList.add('buttonSignIn');
+    sectionLinks.classList.add('sectionLinks');
+    createAccount.classList.add('createAccount');
+    forgotPassword.classList.add('forgotPassword');
+    sectionConnectWith.classList.add('sectionConnectWith');
+    line1.classList.add('line1');
+    othersAccess.classList.add('othersAccess');
+    line2.classList.add('line2');
+    sectionIconApp.classList.add('sectionIconApp');
+    imgIconFacebook.classList.add('imgIconFacebook');
+    imgIconGoogle.classList.add('imgIconGoogle');
+    imgIconTwitter.classList.add('imgIconTwitter')
+
+    imgLogo.setAttribute('src', '../assets/logo-haku-white.png');
+    imgLogo.setAttribute('alt', 'Haku Social Network Logo');
+    form.setAttribute('id', 'signIn-form');
+    inputIconUser.setAttribute('src', '../Assets/icon-user.png')
+    inputIconUser.setAttribute('alt', 'User icon')
+    inputEmail.setAttribute('id', 'inputEmail');
+    inputEmail.setAttribute('type', 'email');
+    inputEmail.setAttribute('placeholder', 'EMAIL');
+    inputEmail.setAttribute('required', '');
+    inputIconPassword.setAttribute('src', '../Assets/icon-password.png');
+    inputIconPassword.setAttribute('alt', 'Secret key icon');
+    inputPassword.setAttribute('id', 'inputPassword');
+    inputPassword.setAttribute('type', 'password');
+    inputPassword.setAttribute('placeholder', 'PASSWORD');
+    inputPassword.setAttribute('minlength', '6');
+    inputPassword.setAttribute('maxlength', '12');
+    inputPassword.setAttribute('required', '');
+    msgEmailPassword.setAttribute('id', 'msg_Email');
+    buttonSingIn.setAttribute('type', 'submit');
+    imgIconFacebook.setAttribute('src', '../assets/icon-facebook.png');
+    imgIconFacebook.setAttribute('alt', 'Facebok logo')
+    imgIconGoogle.setAttribute('src', '../assets/icon-google.png');
+    imgIconGoogle.setAttribute('alt', 'Google logo');
+    imgIconTwitter.setAttribute('src', '../assets/icon-twitter.png');
+    imgIconTwitter.setAttribute('alt', 'Twitter logo');
+
+    buttonSingIn.textContent = 'Sign In';
+    createAccount.innerHTML = `<a href='register' id='linkCreateAccount'>Create Account</a>`
+    forgotPassword.innerHTML = `<a href='password' id='linkForgotPassword'>Forgot your password?</a>`
+    othersAccess.textContent = 'Or connect with';
+
+    containerLogin.append(divBackground, blackTransparency, sectionForm);
+    sectionForm.append(imgLogo, form, sectionLinks, sectionConnectWith, sectionIconApp )
+    form.append(divInputUser, divInputPassword, msgEmailPassword, buttonSingIn);
+    divInputUser.append(spanIconUser, inputEmail);
+    spanIconUser.append(inputIconUser);
+    divInputPassword.append(spanIconPassword, inputPassword);
+    spanIconPassword.append(inputIconPassword);
+    sectionLinks.append(createAccount, forgotPassword);
+    sectionConnectWith.append(line1, othersAccess, line2);
+    sectionIconApp.append(imgIconFacebook, imgIconGoogle, imgIconTwitter);
+
+    return containerLogin;
+};
+
+export default login;
