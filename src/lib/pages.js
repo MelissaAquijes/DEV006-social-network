3ddf5c215c397dd31e9b8bf5519020c2e7e498f1
Melissa Aquijes <melissa.geral10@gmail.com>
2023-07-11 11:13:41 -0500

    Se agrega estilos a la vista de login, imagen de logo.

--- src/lib/pages.js
+++ src/lib/pages.js
@@ -1,19 +1,19 @@
 import login from '../views/login.js';
-import register from '../views/register.js';
-import password from '../views/password.js';
-import home from '../views/home.js';
-import wall from '../views/wall.js';
-import profile from '../views/profile.js'
-import error404 from '../views/error404.js';
+// import register from '../views/register.js';
+// import password from '../views/password.js';
+// import home from '../views/home.js';
+// import wall from '../views/wall.js';
+// import profile from '../views/profile.js'
+// import error404 from '../views/error404.js';
 
 const routes = [
     { path: '/', component: login },
-    { path: '/register', component: register },
-    { path: '/password', component: password },
-    { path: '/home', component: home },
-    { path: '/wall', component: wall},
-    { path: '/profile', component: profile},
-    { path: '/error', component: error404 },
+    // { path: '/register', component: register },
+    // { path: '/password', component: password },
+    // { path: '/home', component: home },
+    // { path: '/wall', component: wall},
+    // { path: '/profile', component: profile},
+    // { path: '/error', component: error404 },
 ]
 
 export {routes}
